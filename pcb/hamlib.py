348079edb17b7a94a042bbdea5bc61ce0bd6b1f4
Cédric HT <altaris@users.noreply.github.com>
2025-01-11 15:30:16 +0900

    Conversion to Qiskit

--- pcb/hamlib.py
+++ pcb/hamlib.py
@@ -63,19 +63,41 @@ def all_hamiltonian_files() -> Generator[tuple[h5py.File, str], None, None]:
     """
     Find all the Hamiltonian files in the HamLib website. And returns them as an
     iterator over tuples of the form `(h5py.File, hamiltonian_file_id)`.
+
+    `h5py.File`'s can essentially be used as dicts, where the keys are names
+    (e.g. `"1-ising2.5-100_5555-10"`) and the byte strings. Each byte string is
+    a serialized sparse Pauli operator that looks like this (after decoding):
+
+        >>> fp["1-ising2.5-100_5555-10"][()].decode("utf-8")
+        22.5 [] +
+        -0.5 [Z9 Z20] +
+        -0.5 [Z9 Z26] +
+        -0.5 [Z9 Z29] +
+        -0.5 [Z9 Z56] +
+
     """
     urls = _all_hamiltonians_urls(_all_index_csv_urls(HAMLIB_URL))
     for url in urls:
-        with TemporaryDirectory() as tmp:
-            response = requests.get(url, stream=True)
-            response.raise_for_status()  # Raise an exception for HTTP errors
-            path = Path(tmp) / url.split("/")[-1]
-            with path.open("wb") as fp:
-                fp.write(response.content)
-            with zipfile.ZipFile(path, mode="r") as fp:
-                assert len(fp.namelist()) == 1
-                name = fp.namelist()[0]
-                hfid = url[len(HAMLIB_URL) : -len(".zip")]
-                with fp.open(name, "r") as h5fp:
-                    with h5py.File(h5fp, "r") as h5fp:
-                        yield h5fp, hfid
+        try:
+            with TemporaryDirectory() as tmp:
+                response = requests.get(url, stream=True)
+                response.raise_for_status()  # Raise an exception for HTTP errors
+                path = Path(tmp) / url.split("/")[-1]
+                with path.open("wb") as fp:
+                    fp.write(response.content)
+                with zipfile.ZipFile(path, mode="r") as fp:
+                    if not (
+                        len(fp.namelist()) == 1
+                        and fp.namelist()[0].endswith(".hdf5")
+                    ):
+                        raise ValueError(
+                            "Expected exactly one .hdf5 file in the "
+                            "downloaded ZIP"
+                        )
+                    name = fp.namelist()[0]
+                    hfid = url[len(HAMLIB_URL) : -len(".zip")]
+                    with fp.open(name, "r") as h5fp:
+                        with h5py.File(h5fp, "r") as h5fp:
+                            yield h5fp, hfid
+        except Exception as e:
+            logging.error("Failed to open Hamiltonian at URL {}: {}", url, e)
